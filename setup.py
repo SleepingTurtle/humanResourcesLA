713fcbc989e150efed80412d72c9c46fe2895314
= <joshcharlestechie@gmail.com>
2018-04-13 20:04:32 +0000

    initial project commit

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup,find_packages
 
+with open('README.rst', encoding='UTF-8') as f:
+    readme = f.read()
+
 setup(
     # Package Info
     name = "hr",
     version = "0.1",
     description = "Manages users on a server based an inventory.json file",
+    long_description = readme,
 
     # Package requirements
     packages = find_packages('src'),
